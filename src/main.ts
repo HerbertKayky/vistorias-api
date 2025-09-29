acd650dae09eba0f45ced0462dcb59c9dff98027
HerbertKayky <herbertkayky9@gmail.com>
2025-09-28 21:31:11 -0300

    feat: adiciona url do front-end em prod

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,8 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:3000', // URL do frontend
-    credentials: true,               // se precisar enviar cookies/headers
+    origin: ['http://localhost:3000', 'https://epta-vistorias.vercel.app'],
+    credentials: true, // se precisar enviar cookies/headers
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   });
   await app.listen(process.env.PORT ?? 8000);
